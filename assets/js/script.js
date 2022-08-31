9c38dee821c5400e1e83246b4acf8f6d0ef73748
crigaux <crigaux057@gmail.com>
2022-08-31 12:46:08 +0200

    landing page mobile + menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,9 +48,142 @@ let menuBtnClose = document.querySelector('.close');
 let overlayMenu = document.querySelector('.overlayMenuBurger');
 
 menuBtnOpen.addEventListener('click', () => {
-    overlayMenu.classList.add('active');
+    overlayMenu.classList.add('open');
+    overlayMenu.classList.remove('close');
 })
 
 menuBtnClose.addEventListener('click', () => {
-    overlayMenu.classList.remove('active');
+    overlayMenu.classList.add('close');
+    overlayMenu.classList.remove('open');
+})
+
+// ##############################################################
+// ###########             Aperçu du menu             ###########
+// ##############################################################
+
+let starter = document.querySelector('#starterPreview');
+let mainDishes = document.querySelector('#mainDishesPreview');
+let dessert = document.querySelector('#dessertPreview');
+let drinks = document.querySelector('#drinksPreview');
+let container = document.querySelector('.catDishesContainer')
+
+starter.addEventListener('click', () => {
+    container.innerHTML =   `<div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Entrée 1</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Entrée 2</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Entrée 3</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Entrée 4</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>`
+})
+mainDishes.addEventListener('click', () => {
+    container.innerHTML = `<div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Plat 1</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Plat 2</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Plat 3</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Plat 4</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>`
+})
+dessert.addEventListener('click', () => {
+    container.innerHTML = `<div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Dessert 1</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Dessert 2</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Dessert 3</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Dessert 4</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>`
+})
+drinks.addEventListener('click', () => {
+    container.innerHTML = `<div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Boisson 1</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Boisson 2</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Boisson 3</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            <div class="catDishiesItem">
+                            <div class="catDishiesItemTitle">
+                                <h4>Boisson 4</h4>
+                                <span>25€</span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>`
 })
